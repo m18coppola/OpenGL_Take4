095ee4166584d346ae6ae21951db6d77ef77d90f
Michael Coppola <m18coppola@gmail.com>
2022-01-13 21:37:02 -0500

    added game loop

--- main.c
+++ main.c
@@ -8,6 +8,14 @@
 int
 main(int argc, char *argv[])
 {
+	GLenum glewError;
+	GLuint program;
+	GLuint vao[1];
+	GLuint vbo[1];
+	float verts[] = {0.0,0.0,0.0,0.0,1.0,0.0,0.5,0.5,0.0,1.0,0.0,0.0};
+	SDL_Event e;
+	int running;
+
 	/* begin SDL2 */
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		fprintf(stderr, "SDL2 failed to initialize. Exiting.\nSDL_Error: %s\n", SDL_GetError());
@@ -37,7 +45,6 @@ main(int argc, char *argv[])
 
 	/* initialize GLEW */
 	glewExperimental = GL_TRUE;
-	GLenum glewError;
 	glewError = glewInit();
 	if (glewError != GLEW_OK) {
 		fprintf(stderr, "Error initializing Glew. Exiting.\nGLEW_Error: %s\n", glewGetErrorString(glewError));
@@ -51,33 +58,42 @@ main(int argc, char *argv[])
 	/* end of initialization */
 
 	/* render*/
-	GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+	program = InitShader("vshader.glsl", "fshader.glsl");
 	glUseProgram(program);
 
 	/* VAO represents a complete rendered object */
-	GLuint vao[1];
 	glGenVertexArrays(1, vao);
 	glBindVertexArray(vao[0]);
 	
 	/* VBO is a allocation of graphics memory */
-	GLuint vbo[1];
 	glGenBuffers(1, vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 
-	/* load data into vram */
-	float verts[] = {0.0,0.0,0.0,0.0,1.0,0.0,0.5,0.5,0.0,1.0,0.0,0.0};
-	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-
-	/* render */
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_TRIANGLES, 0, 12);
+	
+	/* load data into vram */
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
-	/* swap buffers */
-	SDL_GL_SwapWindow(window);
+	/* game loop */
+	running = 1;
+	while (running) {
+		while (SDL_PollEvent(&e) != 0) {
+			if (e.type == SDL_QUIT) {
+				running = 0;
+			}
+		}
+		/* render */
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 12);
+		/* swap buffers */
+		SDL_GL_SwapWindow(window);
+	}
 
-	sleep(5);
-
+	/* clean up */
+	SDL_DestroyWindow(window);
+	window = NULL;
+	glDeleteProgram(program);
 	SDL_Quit();
 	return 0;
 }
